aac777a4ed384549eeefaae508fcf39aee6f1ff5
gabimachado <gmachado@stevens.edu>
2015-11-25 16:31:47 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,34 @@
-//
-//  main.cpp
-//  test_of_pull
-//
-//  Created by songnan on 15/11/17.
-//  Copyright (c) 2015年 songnan. All rights reserved.
-//
+#include "Algebra.h"
 
-#include <iostream>
+vector<Sin*>* Sin::Globalmap = new vector<Sin*>();
+vector<Var*>* Var::Globalmap = new vector<Var*>();
+vector<Cos*>* Cos::Globalmap = new vector<Cos*>();
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+//double const Pi= 3.141592653;
+
+int main() {
+    Expr* e = new Const(5);
+    cout << *e<<endl;   //5
+    
+    cout << e->eval()<<endl; //5
+    
+    Expr* e2 = new Add(new Const(5), new Const(3)); // 5 + 3
+    cout << e2->eval()<<endl; // 5 + 3 =8
+    cout << *e2 << endl;
+    
+    Expr* e3 = new Add(new Var("x"), new Const(3)); // 5 + 3
+    Var::set("x", 5);
+    cout << e3->eval() << endl; // 8
+    cout << *e3 << endl; // x + 3
+    cout << endl;
+    
+    Expr* e4 = new Add(new Add(new Const(3), new Const(4)),new Var("y"));
+    cout << *e4;
+    
+    Expr* e5 = new Sin("x");
+    cout << *e5 << endl;
+    
+    
+    
+    system("pause");
 }
